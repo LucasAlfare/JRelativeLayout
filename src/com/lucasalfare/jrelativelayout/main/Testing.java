10051ea9a1a69f2a150eed09b43adeee2f9bc18c
Lucas Sousa <lucas>
2021-03-22 22:25:37 -0300

    update example in Testing.java

--- src/com/lucasalfare/jrelativelayout/main/Testing.java
+++ src/com/lucasalfare/jrelativelayout/main/Testing.java
@@ -7,19 +7,6 @@ import java.awt.*;
 
 import static com.lucasalfare.jrelativelayout.main.JRelativeLayout.rawConstraints;
 
-/**
- * This should inflate a frame that looks like something:
- * |-----------------------------|
- * | C           A               |
- * |                             |
- * |                             |
- * |                           B |
- * |                             |
- * |                             |
- * |                             |
- * -------------------------------
- */
-
 public class Testing extends JFrame {
 
     public Testing() {
@@ -27,30 +14,25 @@ public class Testing extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        //root layout of the main frame
-        setLayout(new FlowLayout());
+        setLayout(null);
 
-        // creates two example panels
         JPanel p1 = new JPanel();
         p1.setBackground(Color.BLUE);
-        p1.setSize(100, 300);
+        p1.setBounds(0, 0, 100, 300);
 
         JPanel p2 = new JPanel();
         p2.setBackground(Color.GREEN);
-        p2.setSize(100, 300);
+        p2.setBounds(100, 0, 100, 300);
 
-        // creates some components to be added in the panels
         JButton a1 = new JButton("a1");
         JButton b1 = new JButton("b1");
 
         JButton a2 = new JButton("a2");
         JButton b2 = new JButton("b2");
 
-        // define layout manager of the child panels as JRelativeLayout
         p1.setLayout(new JRelativeLayout());
         p2.setLayout(new JRelativeLayout());
 
-        // setup positions of components
         p1.add(a1, rawConstraints().centerVertical());
         p1.add(b1, rawConstraints().below(a1));
 
